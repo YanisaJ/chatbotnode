48c18aa315a5ab678dac4ce236f9b4b6d6dc2504
Yanisa w <mods.loan.assistant@gmail.com>
2021-09-01 18:41:09 +0700

    1/9/64 update 6.5

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -102,7 +102,7 @@ module.exports = (app) => {
         
         const booking = express();
         booking.use(bodyParser.json());
-        booking.post.require('../chatbot/contactBooking');
+        booking.post('../chatbot/contactBooking');
         res.send(booking);
       break;  
       //----------------------------------------------------------------------------
