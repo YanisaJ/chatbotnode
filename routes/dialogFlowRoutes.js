5ff9331102a2031226606fcb74e2ca561e02851d
Yanisa w <mods.loan.assistant@gmail.com>
2021-09-07 18:19:27 +0700

    7/9/64 update 1.5

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -95,8 +95,8 @@ module.exports = (app) => {
       
       //testpath
       case 'a7f0ba63-604b-4087-9485-eb3506d1a700':
-        res.send({
-          fulfillmentText: 'at -> app.post'
+        res.require({
+          fulfillmentText: '../chatbot/chatbot'
         });
         
         // const booking = express();
